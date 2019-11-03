594f2a07b8e7c3eccabce0258c012ab9d0670357
KUHOO-S <kuhoo1999@rediffmail.com>
2019-11-04 01:06:03 +0530

    added

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+var express= require('express');
+var bodyParser=require('body-parser');
+var app=express();
+app.set('view engine','ejs');
+
+var urlencoded=bodyParser.urlencoded({extended:false});
+app.use('/assets',express.static('assets'));
+
+ app.get('/home',function(req,res)
+ {  console.log(req.query);
+     res.render('contact',{qs: req.query});
+ });
+
+ app.post('/contact',urlencoded,function(req,res)
+ {  console.log(req.body);
+     res.render('contact',{qs: req.query});
+ });
+
+app.get('/:nameji',function(req,res){
+//res.sendFile(__dirname+'/hi.html');
+var data={
+    age:70,
+    height:5,
+}
+res.render('index',{nameji:req.params.nameji,data:data});
+});
+
+
+app.listen(3000);
